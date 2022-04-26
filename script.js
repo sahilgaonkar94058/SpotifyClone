843381b286c384157821c4621bf589adbafadbfc
Sahil Sunil Gaonkar <84930040+sahilgaonkar94058@users.noreply.github.com>
2022-04-26 14:03:58 +0530

    Add files via upload
    added pause functionality

--- script.js
+++ script.js
@@ -64,22 +64,36 @@ const makeAllPlays = ()=>{
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{ 
         makeAllPlays();
-        songIndex = parseInt(e.target.id);
-        e.target.classList.remove('fa-play-circle');
-        e.target.classList.add('fa-pause-circle');
-        audioElement.src = `songs/${songIndex+1}.mp3`;
-        masterSongName.innerText = songs[songIndex].songName;
-        audioElement.currentTime = 0;
-        audioElement.play();
-        gif.style.opacity = 1;
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
+        // songIndex = parseInt(e.target.id);
+        // e.target.classList.remove('fa-play-circle');
+        // e.target.classList.add('fa-pause-circle');
+        // audioElement.src = `songs/${songIndex+1}.mp3`;
+        // masterSongName.innerText = songs[songIndex].songName;
+        
+        // audioElement.play();
+        // audioElement.currentTime = 0;
+   
+        // gif.style.opacity = 1;
+        // masterPlay.classList.remove('fa-play-circle');
+        // masterPlay.classList.add('fa-pause-circle');
+        if(audioElement.paused || audioElement.currentTime<=0){
+            audioElement.play();
+            e.target.classList.remove('fa-play-circle');
+            e.target.classList.add('fa-pause-circle');
+            gif.style.opacity = 1;
+        }
+        else{
+            audioElement.pause();
+            e.target.classList.remove('fa-pause-circle');
+            e.target.classList.add('fa-play-circle');
+            gif.style.opacity = 0;
+        }
     })
 })
 
 document.getElementById('next').addEventListener('click', ()=>{
     if(songIndex>=9){
-        songIndex = 0
+        songIndex = 0;
     }
     else{
         songIndex += 1;
@@ -95,7 +109,7 @@ document.getElementById('next').addEventListener('click', ()=>{
 
 document.getElementById('previous').addEventListener('click', ()=>{
     if(songIndex<=0){
-        songIndex = 0
+        songIndex = 0;
     }
     else{
         songIndex -= 1;
